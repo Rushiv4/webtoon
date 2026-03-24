ee3aca85469c59146962b17cc4b2783443abc0ca
Rushikesh Dhane <rdhane4@gmail.com>
2026-03-24 20:39:37 +0530

    update cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // For serving uploaded images
 
